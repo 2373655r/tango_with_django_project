205f217a192fd0226973b231ec9ac1723d6fb889
Rhodri Rees <2373655r@student.gla.ac.uk>
2020-02-11 22:28:02 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -44,6 +46,11 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rango',
 ]
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 MIDDLEWARE = [
@@ -105,6 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    {
+	'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	'OPTIONS': { 'min_length': 6, }
     },
 ]
 
